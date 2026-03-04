0c635f24d5a0f81593b35c48d0f78f97757c5760
Sumit Kalal <sumitkalal128@gmail.com>
2026-03-04 22:22:00 +0530

    rander changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import   register, home_view
 from . import views
-
+from myapp.views import test_api
 urlpatterns = [
     path("register/", register, name="Registration_form"),
     path("login/", views.login_view, name="login_form"),
@@ -9,6 +9,6 @@ urlpatterns = [
     path('users/', views.user_list, name='user_list'),
     path('entries/edit/<int:id>/', views.edit_entry, name='edit_entry'),
     path('entries/delete/<int:id>/', views.delete_entry, name='delete_entry'),
-    path("test/", views.test_api),
+    path("test/", test_api, name="test_api"),
     path("", register, name="Registration_form"),
 ]
