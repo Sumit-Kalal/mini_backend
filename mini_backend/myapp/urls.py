e52b845950ba62eb617d60c0b7e2c2fdce2ee880
Sumit Kalal <sumitkalal128@gmail.com>
2026-03-04 06:34:20 +0530

    inital commit

---
+++ mini_backend/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import   register, home_view
+from . import views
+
+urlpatterns = [
+    path("register/", register, name="Registration_form"),
+    path("login/", views.login_view, name="login_form"),
+    path("dashboard/", home_view, name="dashboard"),
+    path('users/', views.user_list, name='user_list'),
+    path('entries/edit/<int:id>/', views.edit_entry, name='edit_entry'),
+    path('entries/delete/<int:id>/', views.delete_entry, name='delete_entry'),
+    path("", register, name="Registration_form"),
+]
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Week-04, Day-07
+Today's tasks was to create a mini backend project. I just integrated all the tasks from last few days. Login authentication, database creation, CRUD operations.
+- The process remains the same, create server, make database, Write views, forms, models and run the server. Make the frontend page and deploy locally.
+- The main challenge for today is to deploy the system in netlify so the website can be seen by anyone.
